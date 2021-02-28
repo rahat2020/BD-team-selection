ff42d35e784ea38307ef911eec0e2da02dceff68
rahat2020 <bloodyhell4040@gmail.com>
2021-03-01 00:51:34 +0600

    code added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import button from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>this is paragraph</h1>
+        <button className="btn btn-primary">click me</button>
       </header>
     </div>
   );
