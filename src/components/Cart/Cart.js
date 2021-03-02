3bcbd09a8bf3c7b337a225fbc1806070830699f4
rahat2020 <bloodyhell4040@gmail.com>
2021-03-02 12:38:47 +0600

    new add

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,15 +7,21 @@ const Cart = (props) => {
     let name = '';
     for (let i = 0; i < playersCart.length; i++) {
         const players = playersCart[i];
-        name = ` ${name}  ${players.name},` 
+        name = ` ${name }  ${players.name},` 
         total = total + players.salary;
         
+    }
+    let salary = 0;
+    for (let i = 0; i< playersCart.length; i++){
+        const addedPlayer = playersCart[i];
+        salary = addedPlayer.salary;
     }
   
     return (
         <div>
             <h4>Total players selected: {playersCart.length}</h4>
             <h5>Name of players: {name}</h5>
+            <h6>player salary: {salary}</h6>
             <p>Total amount: {total}</p>
 
         </div>
